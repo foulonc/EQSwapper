b0dee180aec9fc2bf9b099d130ed0a8bab63138c
Cedrique Foulon <info@foulonc.be>
2020-12-07 09:54:54 +0100

    refactor + autosize

--- UI/CreateForm.ps1
+++ UI/CreateForm.ps1
@@ -1,5 +1,3 @@
-. .\Code\EQSwapper.ps1
-$count = 0
 # Init PowerShell Gui
 Add-Type -AssemblyName System.Windows.Forms
 # Create a new form
@@ -19,6 +17,8 @@ $ListView.Height = 300
 $ListView.location = new-object system.drawing.point(0, 0)
 $ListView.Columns.Add("Number")
 $ListView.Columns.Add("Headphone")
+$ListView.Add_click( { ListViewAction })
+$ListView.AutoResizeColumns(1)
 
 $loadHeadphoneButton = New-Object system.Windows.Forms.Button
 $loadHeadphoneButton.BackColor = "#a4ba67"
@@ -28,17 +28,7 @@ $loadHeadphoneButton.height = 50
 $loadHeadphoneButton.location = New-Object System.Drawing.Point(0, 250)
 $loadHeadphoneButton.Font = 'Microsoft Sans Serif, 10'
 $loadHeadphoneButton.ForeColor = "#ffffff"
-$loadHeadphoneButton.Add_Click(
-    {    
-        $headphones = get-headphones
-        foreach ($headphone in $headphones) {
-            $item = New-Object System.Windows.Forms.ListViewItem($count)
-            $item.SubItems.Add($headphone.split('\.')[-2])
-            $ListView.Items.AddRange(($item))
-            $count = $count + 1
-        }
-    }
-)
+$loadHeadphoneButton.Add_Click( { ButtonGetHeapdhonesAction })
 
 
 $setHeadphoneEQButton = New-Object system.Windows.Forms.Button
@@ -52,7 +42,6 @@ $setHeadphoneEQButton.ForeColor = "#ffffff"
 
 # ADD OTHER ELEMENTS ABOVE THIS LINE
 # Add the elements to the form
-$EQSwapperForm.controls.AddRange(@( $loadHeadphoneButton, $setHeadphoneEQButton, $ListView))
 
 
 $ListView.Add_click( {
@@ -65,6 +54,34 @@ $ListView.Add_click( {
     })
 
 
+# Logic
+. (Join-Path (Split-Path $PSscriptroot -Parent) "Code\EQSwapper.ps1")
+$count = 0
+$EQSwapperForm.controls.AddRange(@( $loadHeadphoneButton, $setHeadphoneEQButton, $ListView))
+
+
+
+# Functions
+Function ListViewAction {
+    foreach ($item in $ListViewBox.SelectedItems) { 
+        Write-Host $Item.text
+        foreach ($i in $item.SubItems) {
+            Write-Host $i.Text
+        }
+    }
+}
+
+
+function ButtonGetHeapdhonesAction {
+    $headphones = get-headphones
+    foreach ($headphone in $headphones) {
+        $item = New-Object System.Windows.Forms.ListViewItem($count)
+        $item.SubItems.Add($headphone.split('\.')[-2])
+        $ListView.Items.AddRange(($item))
+        $count = $count + 1
+    }
+    
+}
 # THIS SHOULD BE AT THE END OF YOUR SCRIPT FOR NOW
 # Display the form
 [void]$EQSwapperForm.ShowDialog()
